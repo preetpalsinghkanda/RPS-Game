acdb08c6a1fc1885460881a09fb6336b053a6d88
preetpalsinghkanda <preetpalsinghkanda@gmail.com>
2026-07-22 13:32:17 +0530

    added usestate for rules btn

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./component/Navbar";
 import Rules from "./component/Rules";
 import Home from "./component/Home";
 
 const App = () => {
+  const [isRulesOpen, setIsRulesOpen] = useState(false);
+
   return (
     <div className="flex flex-col">
       <Navbar />
-      {/* <Rules/> */}
+      <Rules isRulesOpen={isRulesOpen} setIsRulesOpen={setIsRulesOpen} />
       <Home />
 
-      <button className="border-2 self-end mx-10 border-[#ffffff91] relative bottom-67 tracking-widest font-semibold text-white rounded-lg px-10 py-2">
+      <button
+        onClick={() => setIsRulesOpen(true)}
+        className="border-2 cursor-pointer
+       self-end mx-10 border-[#ffffff91] relative bottom-67 tracking-widest font-semibold text-white rounded-lg px-10 py-2"
+      >
         RULES
       </button>
     </div>
--- src/component/Rules.jsx
+++ src/component/Rules.jsx
@@ -2,18 +2,27 @@ import React from "react";
 import closeIcon from "../assets/icon-close.svg";
 import rulesImg from "../assets/image-rules.svg";
 
-const Rules = () => {
+const Rules = ({ isRulesOpen, setIsRulesOpen }) => {
   return (
-    <div className="fixed inset-0 flex justify-center items-center bg-black/50">
-      <div className="bg-white px-12 rounded-xl  flex flex-col  gap-10 py-8">
-        <div className="flex items-center justify-between">
-          <h4 className="font-bold text-3xl text-[hsl(229,25%,31%)]">RULES</h4>
-          <img className="cursor-pointer" src={closeIcon} alt="" />
-        </div>
+    isRulesOpen && (
+      <div className="fixed z-15 inset-0 flex justify-center items-center bg-black/50">
+        <div className="bg-white px-12 rounded-xl  flex flex-col  gap-10 py-8">
+          <div className="flex items-center justify-between">
+            <h4 className="font-bold text-3xl text-[hsl(229,25%,31%)]">
+              RULES
+            </h4>
+            <img
+              onClick={() => setIsRulesOpen(false)}
+              className="cursor-pointer"
+              src={closeIcon}
+              alt=""
+            />
+          </div>
 
-        <img className="" src={rulesImg} alt="" />
+          <img className="" src={rulesImg} alt="" />
+        </div>
       </div>
-    </div>
+    )
   );
 };
 
